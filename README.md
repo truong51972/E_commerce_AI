2824d05e5f528d83f1f303d54b0993914013d0db
truong51972 <tranquoctruong20@gmail.com>
2025-01-18 23:45:04 +0700

    create frontend and user service

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # E_commerce_AI
+
+
+# script inscrease buffer size
+# create in <username>/.wslconfig
+# coppy below
+# ######COPY######
+# [wsl2]
+# kernelCommandLine = "sysctl.net.core.rmem_max=8388608 sysctl.net.core.wmem_max=8388608"
---
+++ run_all_services.sh
@@ -0,0 +1,4 @@
+docker network create microservices-network
+
+docker compose -f ./frontend-service/docker-compose.yml -f ./docker-compose.override.yml up -d
+docker compose -f ./user-service/docker-compose.yml -f ./docker-compose.override.yml up -d
