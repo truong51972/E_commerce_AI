731d498700f2a04d0a7116c707dcac4a5906f95f
truong51972 <tranquoctruong20@gmail.com>
2025-02-16 16:19:18 +0700

    update all docker compose config file

---
+++ user_service/backend/gunicorn.conf.py
@@ -0,0 +1,51 @@
+# gunicorn.conf.py
+import multiprocessing
+import os
+
+
+bind = "0.0.0.0:8000"
+
+# number of worker processes
+if os.getenv("GUNICORN_WORKERS") == 'max':
+    workers = multiprocessing.cpu_count() * 2 + 1 
+else:
+    try:
+        workers = int(os.getenv("GUNICORN_WORKERS"))
+    except:
+        workers = 1
+
+if os.getenv("GUNICORN_THREADS") == 'max':
+    threads = multiprocessing.cpu_count() * 2 + 1 
+else:
+    try:
+        threads = int(os.getenv("GUNICORN_THREADS"))
+    except:
+        threads = 2
+
+# reload = True
+# Worker class
+worker_class = 'gevent'  # use gevent for async IO
+
+# threads for worker
+
+
+# Timeouts
+timeout = 120
+keepalive = 5
+graceful_timeout = 120
+
+# limit requests
+max_requests = 1000
+max_requests_jitter = 200
+
+# Buffer size
+worker_connections = 1000
+
+# Logging
+accesslog = '-'
+errorlog = '-'
+loglevel = 'info'
+
+
+# Monitoring
+# statsd_host = 'localhost:8125'
