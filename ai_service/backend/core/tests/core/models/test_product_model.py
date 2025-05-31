d99232d7b955ca943d85558fe298835da9aea177
truong51972 <tranquoctruong20@gmail.com>
2025-05-31 15:37:04 +0700

    feat: add unit tests for product model

---
+++ ai_service/backend/core/tests/core/models/test_product_model.py
@@ -0,0 +1,67 @@
+import dotenv
+import pandas as pd
+from pymilvus import utility
+
+import pytest
+from core.base.base_milvus import BaseMilvus
+from core.models.product.product_model import ProductModel
+
+dotenv.load_dotenv()
+
+
+collection_name = "test_product_collection"
+llm_model = "gemini-2.0-flash"
+milvus_uri = "http://localhost:19530"
+milvus_token = "root:Milvus"
+
+
+@pytest.fixture(scope="module", autouse=True)
+def setup_milvus_collection():
+    # check if the collection already exists and delete it
+    BaseMilvus(
+        collection_name=collection_name,
+        milvus_uri=milvus_uri,
+        milvus_token=milvus_token,
+    )
+
+    if utility.has_collection(collection_name):
+        utility.drop_collection(collection_name)
+
+
+def test_product_model_initialization():
+    """Test the initialization of the ProductModel class."""
+
+    # Initialize the ProductModel with test parameters
+
+    product_model = ProductModel(
+        collection_name=collection_name,
+        llm_model=llm_model,
+        milvus_uri=milvus_uri,
+        milvus_token=milvus_token,
+    )
+
+    assert product_model.collection_name == collection_name
+    assert product_model.llm_model == llm_model
+    assert product_model.milvus_uri == milvus_uri
+    assert product_model.milvus_token == milvus_token
+
+    # utility.drop_collection(collection_name)
+
+
+def test_product_model_create():
+    """Test the create method of the ProductModel class."""
+
+    product_model = ProductModel(
+        collection_name=collection_name,
+        llm_model=llm_model,
+        milvus_uri=milvus_uri,
+        milvus_token=milvus_token,
+    )
+
+    df = pd.read_excel("core/tests/core/models/data_test.xlsx")
+
+    df.columns = df.columns.str.lower()
+
+    data = df.to_dict(orient="records")
+
+    product_model.create_records(data)
